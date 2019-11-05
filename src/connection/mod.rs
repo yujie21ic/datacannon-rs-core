0761c054af60f52ef825f6b49643a7dec8cb129e
Andrew Evans <asevans48@gmail.com>
2019-11-05 16:53:03 -0700

    moved connection here

---
+++ src/connection/mod.rs
@@ -0,0 +1,7 @@
+pub mod connection;
+pub mod rabbitmq_connection_factory;
+pub mod rabbitmq_connection_utils;
+pub mod rabbitmq_connection_pool;
+pub mod threadable_rabbit_mq_connection;
+pub mod rabbitmq_channel_utils;
+pub mod pool_errors;
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ pub mod amqp;
 pub mod argparse;
 pub mod backend;
 pub mod config;
+pub mod connection;
 pub mod message_protocol;
 pub mod nodename;
 pub mod serde_utils;
